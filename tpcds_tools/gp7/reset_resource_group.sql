6740970b5f4730adadf0ba1245c2d3f451af7eee
Brandon Tat <tatb@vmware.com>
2023-06-14 14:11:43 -0400

    GPV-1576 Fix resource group and GUC setting for GP7
    - We make sure to setup the max_statement_mem before statement_mem, so that
    `gpstart` can start the cluster. Otherwise, if the current `max_statement_mem`
    is less than the `statement_mem`, then postmaster will be failed to start.
    
    - We ensure the `gp_resource_group_cpu_limit` to 0.9, since we will set the
    CPU_HARD_QUOTA_LIMIT to 90 percent. If we over set the RG configuration, then
    cluster can also failed to start.
    
    - We put down the default RG configuration in the script.
    
    Co-authored-by: Brandon Tat <tatb@vmware.com>
    Co-authored-by: Xin Zhang <zhxin@vmware.com>

--- tpcds_tools/gp7/reset_resource_group.sql
+++ tpcds_tools/gp7/reset_resource_group.sql
@@ -1,7 +1,19 @@
 alter resource group default_group set CONCURRENCY 20;
-alter resource group default_group set CPU_HARD_QUOTA_LIMIT 45;
-alter resource group default_group set MEMORY_LIMIT 3000;
+alter resource group default_group set CPU_HARD_QUOTA_LIMIT 20;
+alter resource group default_group set CPU_SOFT_PRIORITY 100;
+alter resource group default_group set MEMORY_LIMIT -1;
+alter resource group default_group set MIN_COST 500;
 
 alter resource group admin_group set CONCURRENCY 10;
+alter resource group admin_group set CPU_HARD_QUOTA_LIMIT 10;
+alter resource group admin_group set CPU_SOFT_PRIORITY 100;
 alter resource group admin_group set MEMORY_LIMIT -1;
+alter resource group admin_group set MIN_COST 500;
+
+alter resource group system_group set CONCURRENCY 0;
+alter resource group system_group set CPU_HARD_QUOTA_LIMIT 10;
+alter resource group system_group set CPU_SOFT_PRIORITY 100;
+alter resource group system_group set MEMORY_LIMIT -1;
+alter resource group system_group set MIN_COST 500;
+
 SELECT * FROM gp_toolkit.gp_resgroup_config;
--- tpcds_tools/gp7/set_resource_group.sql
+++ tpcds_tools/gp7/set_resource_group.sql
@@ -1,8 +1,10 @@
 alter resource group default_group set CONCURRENCY 5;
-alter resource group default_group set CPU_HARD_QUOTA_LIMIT 95;
+alter resource group default_group set CPU_HARD_QUOTA_LIMIT 90;
 alter resource group default_group set MEMORY_LIMIT 25000;
 
 alter resource group admin_group set CONCURRENCY 5;
-alter resource group admin_group set CPU_HARD_QUOTA_LIMIT 95;
+alter resource group admin_group set CPU_HARD_QUOTA_LIMIT 90;
 alter resource group admin_group set MEMORY_LIMIT 25000;
+
+alter resource group system_group set CPU_HARD_QUOTA_LIMIT 90;
 SELECT * FROM gp_toolkit.gp_resgroup_config;
